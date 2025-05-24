76b122cc31b16e9bff72eaffc47de4bc0fbfa1e3
kattrin0 <164199593+kattrin0@users.noreply.github.com>
2025-05-24 17:43:38 +0300

    add graph

--- src/ru/vsu/cs/averkieva/Main.java
+++ src/ru/vsu/cs/averkieva/Main.java
@@ -1,5 +1,5 @@
 package ru.vsu.cs.averkieva;
-
+import static ru.vsu.cs.averkieva.HashMapGraph.saveAndVisualize;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,5 +29,24 @@ public class Main {
         System.out.println( map.containsKey("1234"));
         map.printMap();
         System.out.println("size: " + map.size());*/
+
+        HashMap<String, String> map = new HashMap<>();
+
+        // Добавляем тестовые данные
+       /* map.put("Key1", 1);
+        map.put("Key2", 2);
+        map.put("Key3", 3);
+        map.put("Key4", 4);
+        map.put("Key5", 5);*/
+        map.put("1234", "cat");
+        map.put("1345", "dog");
+        map.put("1234", "flower");
+        map.put("1563", "for test");
+        map.put("a", "for test1");
+
+
+        saveAndVisualize(map,"hashmap.dot");
+
+        //System.out.println("Визуализация сохранена в hashmap.png");
     }
 }
